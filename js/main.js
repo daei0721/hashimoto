3cd1d24fea734a5f71bac305e6251afc218dbb40
daei <daeisama721@yahoo.co.jp>
2020-12-12 19:59:45 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    loop:true,
+    slidesPerView:3.55,//表示枚数
+    spaceBetween:115,//画像の感覚
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    }
+});
